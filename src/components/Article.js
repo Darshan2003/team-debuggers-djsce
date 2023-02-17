ae1fad32f2f8b0a4e473e41f3fa1f5bfbd088d2e
rohan9024 <rohanwandre24@gmail.com>
2023-02-18 01:04:13 +0530

    pushed the code again for aatmasamadhaan

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from "react-router-dom"
 import news1 from "../assets/news1.jpeg"
 
-const Article = () => {
+const Article = ({ headline, image, desc }) => {
     return (
         <div>
             <NavLink to="/">
@@ -32,12 +32,12 @@ const Article = () => {
                                     className='sm:w-50 md:w-75 lg:w-100 object-contain h-100 mx-auto cursor-pointer'
                                     src={news1} alt="logo"
                                 />
-                                <p className="text-black mt-3 text-md leading-tight font-light mt-5 sm:mx-5 md:mx-10 lg:mx-20">
-                                Mumbai: Former Maharashtra Chief Minister Uddhav Thackeray launched a strong attack on his rival Eknath Shinde, 
-                                calling him a "traitor who won't change", after the Election Commission today allowed Team Shinde to keep the 
-                                Shiv Sena name and the party's bow and arrow symbol. The Election Commission's decision comes nearly eight months 
-                                after Mr Shinde, the current Chief Minister, revolted with over 40 Sena MLAs and wrested control of the party from 
-                                Mr Thackeray's alliance government with the Congress and the Nationalist Congress Party.
+                                <p className="text-black  text-md leading-tight font-light mt-5 sm:mx-5 md:mx-10 lg:mx-20">
+                                    Mumbai: Former Maharashtra Chief Minister Uddhav Thackeray launched a strong attack on his rival Eknath Shinde,
+                                    calling him a "traitor who won't change", after the Election Commission today allowed Team Shinde to keep the
+                                    Shiv Sena name and the party's bow and arrow symbol. The Election Commission's decision comes nearly eight months
+                                    after Mr Shinde, the current Chief Minister, revolted with over 40 Sena MLAs and wrested control of the party from
+                                    Mr Thackeray's alliance government with the Congress and the Nationalist Congress Party.
                                 </p>
                             </div>
                         </div>
