26d22365af22e2a7480fb4e18950144a8f0c2251
rohan9024 <rohanwandre24@gmail.com>
2023-02-17 13:33:04 +0530

    Again adding pages

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './Header'
 
 function Homepage() {
   return (
-    <div style={{fontFamily: 'Light'}}>
-     Homepage
+    <div style={{ fontFamily: 'Light' }}>
+
+      <Header />
     </div>
   )
 }
